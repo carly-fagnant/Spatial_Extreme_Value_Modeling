60f0d198a307e480e66912a52b1e581b8809d77c
eduardolandin <55931257+eduardolandin@users.noreply.github.com>
2021-07-01 11:33:35 -0500

    Update functionV2.R

--- Test/functionV2.R
+++ Test/functionV2.R
@@ -10,7 +10,7 @@ library(spdep)
 # Notes on key changes ----------------------------------------------------
 
 # extHaus
-# Check the type of input A and B to determine how to perform the extended hausdorff distance calculations
+# Added checks to determine the type of A and B in order to decide how to perform the extended hausdorff distance calculations
 # Extended the function's capabilities to handle {point, point}, {point, line}, {point, area}, {line, line}, {line, area}, and {area, area} calculations
 # In the cases where f1 == 1 or f2 == 1, replaced the calls to gDistance(A, B, hausdorff=T) with calls to directHaus(A, B, 1) and directHaus(B, A, 1) (respectively)
   # gDistance(A, B, hausdorff=T) returns the value of H(A, B) whereas in the cases above we are actually interested in h(A, B) (when f1 == 1) and h(B, A) (when f2 == 1)
@@ -21,7 +21,7 @@ library(spdep)
 # Allowed directHaus to perform computations when f1=1 instead of throwing an error
 # Extendeded the function's capabilities to handle line-to-area, line-to-line, line-to-point, area-to-area, area-to-line, and area-to-point calculations
 # The output was originally a list, of one number. It now directly outputs a numeric value so that it doesn't have to be accessed by $direct.haus[1] in the other functions.
-# Added check if the regions are the same - should 0 be returned?
+# Added check if the regions are the same. At the moment 0 is returned but should 0 be returned?
 # Added a case for f1=0 (simply return the minimum distance between A and B instead of performing the epsilon-buffer procedure)
 
 # pointHaus
@@ -35,16 +35,16 @@ library(spdep)
 # hausMat -----------------------------------------------------------------
 #' Creating a matrix of (extended) Hausdorff distances
 #' 
-#' This function takes a SpatialPolygons object 
+#' This function takes a SpatialPolygons object and a decimal in [0, 1].
 #'  
 #'@param shp a SpatialPolygons object.
 #'@param f1 The percentage (as a decimal) of region i to retain when calculating the directional Hausdorff distance from region i to region j.
 #'@param f2 The percentage (as a decimal) of region j to retain when calculating the directional Hausdorff distance from region j to i. Defaults to the value of f1. Note that specifying a different value will result in a non-symmetric matrix.
 #'@param fileout Should the resulting weight matrix be written to file? Defaults to FALSE 
-#'@param filename If \code{fileout} is TRUE, the name for the file to be outputted.
-#'@param ncores If \code{do.parallel} is true, the number of cores to be used for parallel computation. Default is 1. 
-#'@param timer If T, records \code{Sys.time()} when the function is called and outputs the elapsed time along with the matrix. Default is F.
-#'@param do.parallel If T, uses the number of cores specified using \code{ncores} to set up a cluster with the \code{foreach} package. 
+#'@param filename If "fileout" is TRUE, the name for the file to be outputted.
+#'@param ncores If "do.parallel" is true, the number of cores to be used for parallel computation. Default is 1. 
+#'@param timer If T, records "Sys.time()" when the function is called and outputs the elapsed time along with the matrix. Default is F.
+#'@param do.parallel If T, uses the number of cores specified using "ncores" to set up a cluster with the "foreach" package. 
 #'  
 #'@return an nxn matrix of requested distances.
 hausMat <- function(shp, f1, f2=f1, fileout=FALSE, filename=NULL, ncores=1, timer=F, do.parallel=T) {
@@ -96,8 +96,8 @@ hausMat <- function(shp, f1, f2=f1, fileout=FALSE, filename=NULL, ncores=1, time
 #'
 #'@param A region/line/point used to calculate the extended Hausdorff distance: SpatialPolygons or SpatialLines or SpatialPoints
 #'@param B region/line/point used to calculate the extended Hausdorff distance: SpatialPolygons or SpatialLines or SpatialPoints
-#'@param f1 the percentage of area/length in B you want captured as a decimal (eg 10\% = .1)
-#'@param f2 the percentage of area/length in A you want captured as a decimal (eg 10\% = .1). Defaults to the same value as f1.
+#'@param f1 the percentage of area/length in B you want captured as a decimal (eg 10% = .1)
+#'@param f2 the percentage of area/length in A you want captured as a decimal (eg 10% = .1). Defaults to the same value as f1.
 #'@param tol value to be passed to "tol" in "directHaus".
 #'@return the extended hausdorff distance (max of directional from A to B and B to A)
 #'
@@ -108,7 +108,7 @@ extHaus <- function(A, B, f1, f2=f1, tol=NULL) {
   }
   if (is.null(gDifference(A, B))) {
     return(0)
-  } ## check if two regions are the same, if so return HD of zero.
+  } # check if two regions are the same, if so return a hausdorff distance of zero.
   
   # get the class of A and B to perform calculations accordingly
   type.of.A <- class(A)[1]
@@ -118,6 +118,7 @@ extHaus <- function(A, B, f1, f2=f1, tol=NULL) {
     # if both A and B are points, return the cartesian minimum distance between them
     return (gDistance(A, B))
   } else if (type.of.A == "SpatialPoints") {
+    # if one of A or B are points, delegate to pointHaus
     return(pointHaus(A, B, f2, tol=tol))
   } else if (type.of.B == "SpatialPoints") {
     return(pointHaus(B, A, f1, tol=tol))
@@ -157,12 +158,12 @@ extHaus <- function(A, B, f1, f2=f1, tol=NULL) {
 # directHaus --------------------------------------------------------------
 #' Calculate the directional extended Hausdorff Distance
 #' 
-#' This function takes a SpatialPolygons object 
+#' This function takes a SpatialPolygons/SpatialLines object, a SpatialPolygons/SpatialLines/SpatialPoints object and a decimal in [0, 1]
 #'  
 #'@author Julia Schedler
 #'
-#'@param A region/line used to calculate the directed extended Hausdorff distance: SpatialPolygons or SpatialLine
-#'@param B region/line/point used to calculate the directed extended Hausdorff distance: SpatialPolygons or SpatialLine or SpatialPoints
+#'@param A region/line used to calculate the directed extended Hausdorff distance: SpatialPolygons or SpatialLines
+#'@param B region/line/point used to calculate the directed extended Hausdorff distance: SpatialPolygons or SpatialLines or SpatialPoints
 #'@param f1 the percentage of area in A you want captured as a decimal (eg 10\% = .1)
 #'@param tol tolerance for selecting the epsilon buffer to yield desired f1. Default is 1/10000th the sampled directional distances.
 #'
@@ -182,19 +183,19 @@ directHaus <- function(A, B, f1, tol=NULL) {
   # generate points
   n <- 10000
   a.coords <- spsample(A, n=n, type="regular") # sample points within A
-  # should be able to work with both regions and lines: https://cran.r-project.org/web/packages/sp/sp.pdf
+  # sp::spsample should work with both polygons and lines: https://cran.r-project.org/web/packages/sp/sp.pdf
   # compute minimum distance of points a.coords to a point in B
   dists <- gDistance(a.coords, B, byid=T)
   
   if(is.null(tol)){
     tol <- sd(dists[1,]) / 100
   }
-  ## find desired quantile of distances
+  # find desired quantile of distances
   epsilon <- as.numeric(quantile(dists[1,], f1)) #buffer width
   buff <- buffer(B, width=epsilon, dissolve=T) 
-  # raster package, should be point/line/polygon friendly: https://www.rdocumentation.org/packages/raster/versions/3.4-13/topics/buffer
+  # raster::buffer should be point/polgon/line friendly
   overlap.region <- gIntersection(buff, A) # overlap region of buffer+B with A
-  # should be spatialPolygon friendly https://cran.rstudio.com/web/packages/rgeos/rgeos.pdf
+  # rgeos:gIntersection should be polygon/line friendly https://cran.rstudio.com/web/packages/rgeos/rgeos.pdf
   
   type.of.A <- class(A)[1]
   # calculate fraction of A in the overlap region
@@ -243,13 +244,13 @@ directHaus <- function(A, B, f1, tol=NULL) {
 # pointHaus ---------------------------------------------------------------
 #' Calculate the extended Hausdorff Distance from a point to an area
 #' 
-#' This function takes a SpatialPolygons object 
+#' This function takes a SpatialPoints object, a SpatialLines/SpatialPolygons objec, and a decimal in [0, 1].
 #'  
 #'@author ???
 #'
 #'@param point a SpatialPolints object representing a point
 #'@param B region calculate the extended Hausdorff distance: SpatialPolygons or SpatialLines
-#'@param f2 the percentage of area in B you want captured as a decimal (eg 10\% = .1)
+#'@param f2 the percentage of area in B you want captured as a decimal (eg 10% = .1)
 #'@param tol tolerance for selecting the epsilon buffer to yield desired f2. Default is NULL.
 #'
 #'@return the extended hausdorff distance between point and B
