be6e503ce3b643ef5b59a178d35ab4c69e53495f
carly-fagnant <66962195+carly-fagnant@users.noreply.github.com>
2021-09-03 18:06:15 -0500

    Update Testing_on_Simulated_Data.R

--- Code/Testing_on_Simulated_Data.R
+++ Code/Testing_on_Simulated_Data.R
@@ -1,7 +1,50 @@
-
-
+#######################
+# Testing the models on simulated data
+#
+# by Carly Fagnant
+#######################
+library(extRemes)
+library(rgeos)
+library(rgdal)
+library(sp)
+library(gstat)
+library(dplyr)
+library(spdep)
+library(spatialreg)
+library(foreach)
 
 thresh = 253
+
+# grid_regs_df <- readRDS(file = "~/Documents/GitHub/Spatial_Extreme_Value_Modeling/Data/grid_regs_df.rds")
+
+# Function to project data given...
+#     df: a data frame with data and coordinates
+project_data <- function(df){
+  coordinates(df)=~long+lat
+  proj4string(df) <- "+proj=longlat +datum=WGS84"
+  df <- spTransform(df, CRS("+init=epsg:2278")) # projecting data
+  is.projected(df)
+  return(df)
+}
+
+proj4string(ws_regs)
+proj4string(grid_regs_df)
+
+grid_regs <- grid_regs_df
+coordinates(grid_regs)=~x1+x2
+# proj4string(df) <- "+proj=longlat +datum=WGS84"
+proj4string(grid_regs) <- "+init=epsg:2278"
+# grid_regs <- spTransform(grid_regs, CRS("+init=epsg:2278")) # projecting data
+is.projected(grid_regs)
+proj4string(grid_regs) == proj4string(ws_regs)
+
+test_int <- sp::over(grid_regs, ws_regs)
+test_int$REGION <- as.factor(test_int$REGION)
+test_h <- model.matrix( ~ REGION - 1, data=test_int)
+test_inds <- as.integer(rownames(test_h))
+
+
+
 # stations <- read.csv("~/Documents/GitHub/Spatial_Extreme_Value_Modeling/Data/station_info.csv")
 # stat_nos <- stations[,1]
 
@@ -28,23 +71,29 @@ format_data_from_sim <- function(sim_data, reg_polygons, grid_regs_df){
   }
   
   grid_regs_df <- grid_regs_df %>%
-    dplyr::mutate(scale = ws_scale, shape = ws_shape, rate = ws_rate, long = LONGITUDE, lat = LATITUDE)
-  
-  # add new location (long/lat) columns that we will transform to coordinates, as well as parameter values
-  stations_df <- stations %>%
-    dplyr::mutate(scale = ws_scale, shape = ws_shape, rate = ws_rate, long = LONGITUDE, lat = LATITUDE)
-  
-  stations <- project_data(stations_df)
-  region_int <- sp::over(stations, reg_polygons)
-  region_int$REGION <- as.factor(region_int$REGION)
-  h <- model.matrix( ~ REGION - 1, data=region_int)
-  inds <- as.integer(rownames(h))
-  
-  stations_sub_df <- stations_df %>%
-    dplyr::filter(STAT_NO %in% inds) %>%
+    dplyr::mutate(scale = ws_scale, shape = ws_shape, rate = ws_rate) %>%
     dplyr::filter(!is.na(scale))
+  grid_regs <- grid_regs_df
+  coordinates(grid_regs)=~x1+x2
+  proj4string(grid_regs) <- "+init=epsg:2278"
   
-  stations_sub <- project_data(stations_sub_df)
+  # already has Region information!
+  
+  # # add new location (long/lat) columns that we will transform to coordinates, as well as parameter values
+  # stations_df <- stations %>%
+  #   dplyr::mutate(scale = ws_scale, shape = ws_shape, rate = ws_rate, long = LONGITUDE, lat = LATITUDE)
+  # 
+  # stations <- project_data(stations_df)
+  # region_int <- sp::over(stations, reg_polygons)
+  # region_int$REGION <- as.factor(region_int$REGION)
+  # h <- model.matrix( ~ REGION - 1, data=region_int)
+  # inds <- as.integer(rownames(h))
+  # 
+  # stations_sub_df <- stations_df %>%
+  #   dplyr::filter(STAT_NO %in% inds) %>%
+  #   dplyr::filter(!is.na(scale))
+  # 
+  # stations_sub <- project_data(stations_sub_df)
   
   ## Have to redo intersect, because now NAs are removed. 
   ## If remove them before the intersect, the station indicies get messed up. So have to do intersect both before and after
